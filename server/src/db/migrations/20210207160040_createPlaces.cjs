794c1e1eb600fbfe47b81a2539f255008a39f960
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-07 17:20:12 -0500

    add places table migration

---
+++ server/src/db/migrations/20210207160040_createPlaces.cjs
@@ -0,0 +1,31 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("places", (table) => {
+    table.bigIncrements("id").primary()
+    table.string("name").notNullable()
+    table.string("location").notNullable()
+    table.string("rating")
+    table.string("image")
+    table.string("price")
+    table.string("url")
+    table.json("location")
+    table.json("hours")
+    table.string("yelpId")
+    table.json("reviews")
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+  })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = async (knex) => {
+  return knex.schema.dropTableIfExists("places")
+}
