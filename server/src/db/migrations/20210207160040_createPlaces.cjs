60213d30109c08a31c2f50644b2ab5cc6307c576
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-08 16:36:35 -0500

    re-added reviews table migration, deleted notes

--- server/src/db/migrations/20210207160040_createPlaces.cjs
+++ server/src/db/migrations/20210207160040_createPlaces.cjs
@@ -7,16 +7,15 @@
  */
 exports.up = async (knex) => {
   return knex.schema.createTable("places", (table) => {
-    table.bigIncrements("id").primary() //uuid?
+    table.bigIncrements("id").primary()
     table.string("name").notNullable()
-    table.json("type") //arr?
+    table.string("type")
     table.string("address").notNullable()
     table.integer("rating")
     table.string("image")
     table.string("price")
     table.string("url")
-    table.json("hours")
-    table.json("externalReviews")
+    table.string("hours")
     table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
     table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
   })
