5e0605ec4d045da610c81bdd1e3af961bef3dd82
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-12 10:10:16 -0500

    deleted some noted out fluff-code

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -43,23 +43,3 @@ const TopBar = ({ user }) => {
 };
 
 export default TopBar;
-
-{/* <div class="top-bar topbar-center-logo" id="topbar-center-logo">
-  <div class="top-bar-left">
-    <ul class="menu vertical medium-horizontal">
-      <li><a href="#">One</a></li>
-      <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
-    </ul>
-  </div>
-  <div class="top-bar-center">
-    <a href="#"><img src="https://placehold.it/100x39" alt="" /></a>
-  </div>
-  <div class="top-bar-right">
-    <ul class="menu vertical medium-horizontal">
-      <li><a href="#">Four</a></li>
-      <li><a href="#">Five</a></li>
-      <li><a href="#">Six</a></li>
-    </ul>
-  </div>
-</div> */}
