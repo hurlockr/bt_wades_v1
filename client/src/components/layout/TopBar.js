8452e38d963260b3c83f1e1e402c0b700d1d7420
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-12 09:51:20 -0500

    updated styling to the TopBar

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -21,20 +21,45 @@ const TopBar = ({ user }) => {
   ];
 
   return (
-    <div className="top-bar">
+    <div className="top-bar topbar-center-logo" id="topbar-center-logo">
       <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">App</li>
+        <ul className="menu vertical medium-horizontal">
+          {/* <li className="menu-text">The Wonderful Horrible Dates App</li> */}
           <li>
             <Link to="/">Home</Link>
           </li>
         </ul>
       </div>
+      <div className="top-bar-center">
+        <div className="logo">
+          <a><img src="https://placehold.it/100x39" alt="Wades Logo" /></a>
+        </div>
+      </div>
       <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
+        <ul className="menu vertical medium-horizontal">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
       </div>
     </div>
   );
 };
 
 export default TopBar;
+
+{/* <div class="top-bar topbar-center-logo" id="topbar-center-logo">
+  <div class="top-bar-left">
+    <ul class="menu vertical medium-horizontal">
+      <li><a href="#">One</a></li>
+      <li><a href="#">Two</a></li>
+      <li><a href="#">Three</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-center">
+    <a href="#"><img src="https://placehold.it/100x39" alt="" /></a>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu vertical medium-horizontal">
+      <li><a href="#">Four</a></li>
+      <li><a href="#">Five</a></li>
+      <li><a href="#">Six</a></li>
+    </ul>
+  </div>
+</div> */}
