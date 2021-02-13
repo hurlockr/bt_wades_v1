44d682ef3cf27adaa9f27eac27353bbbe4bcb20c
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-13 15:33:19 -0500

    deleted places router to avoid potenial merge conflict

--- server/src/apiClient/YelpClient.js
+++ server/src/apiClient/YelpClient.js
@@ -7,7 +7,7 @@ const yelpApiKey = config.yelpApiKey.secret
 class YelpClient {
   static async getPlaces() {
     try {
-      const url = `https://api.yelp.com/v3/businesses/search?location=Boston`
+      const url = `https://api.yelp.com/v3/businesses/search?`
       const apiResponse = await got(url, {
         'headers': {
           'Authorization': `Bearer ${yelpApiKey}`
