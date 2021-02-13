9f2d87adf684379319f6e2d16e834acaaee93cd9
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-13 17:51:02 -0500

    added all assets to PlaceTile return

--- client/src/components/PlaceTile.js
+++ client/src/components/PlaceTile.js
@@ -1,10 +1,18 @@
 import React from "react"
 
 const PlaceTile = (props) => {
-  console.log(props)
+  const { distance, id, image, location, name, price, rating } = props
+  const getLocation = location.join(' ')
   
   return (
-    <h1>Hello from Place Tile!</h1>
+    <div className="place-tile">
+      <h2>{name}</h2>
+      <img src={image}/>
+      <p>{price}</p>
+      <p>{rating}</p>
+      <p>{getLocation}</p>
+      <p>{distance}</p>
+    </div>
   )
 }
 
