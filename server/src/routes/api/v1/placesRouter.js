7af024e16ee05d37f9fff415d4e53dc7a871b4fd
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-13 15:20:11 -0500

    deleted potentially troublesome code from places router and the YelpRequest file

--- server/src/routes/api/v1/placesRouter.js
+++ server/src/routes/api/v1/placesRouter.js
@@ -3,8 +3,5 @@ import express from "express"
 
 const placesRouter = new express.Router()
 
-// placesRouter.post("/", async (req, res) => {
-//   const locations = req.body 
-// })
 
 export default placesRouter
