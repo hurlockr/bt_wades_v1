9bd6118d8c8d2c19a5b3bf69bf1a52f720813e73
Ross Hurlock <jrlhurlock@gmail.com>
2021-03-03 16:32:01 -0500

    added redirect to places page in test 1

--- e2e/cypress/integration/01_yelpSearchForm.spec.js
+++ e2e/cypress/integration/01_yelpSearchForm.spec.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
-context("Yelp Search Form", () => {
-  beforeEach(() => {
-    cy.visit("/")
-  })
+context('Yelp Search Form', () => {
+  it('Redirects user to places page', () => {
+    cy.visit('/')
 
+    cy.get('.button1').click()
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/places')
+    })
+  })
+  
 })
