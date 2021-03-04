14dbfe3cd272cf05c8885019796b2d9a7c82482f
Ross Hurlock <jrlhurlock@gmail.com>
2021-03-04 17:04:32 -0500

    added testing for all yelpForm inputs

--- e2e/cypress/integration/01_yelpSearchForm.spec.js
+++ e2e/cypress/integration/01_yelpSearchForm.spec.js
@@ -1,14 +1,40 @@
 /// <reference types="cypress" />
 
 context('Yelp Search Form', () => {
-  it('Redirects user to places page', () => {
+  it('accepts zip code input', () => {
+    const zip = '02215'
     cy.visit('/')
 
-    cy.get('.button1').click()
+    cy.get('#zip')
+      .type(zip)
+      .should('have.value', zip)
+  })
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/places')
-    })
+  it('accepts term input', () => {
+    const term = 'Starbucks'
+    cy.visit('/')
+
+    cy.get('#term')
+      .type(term)
+      .should('have.value', term)
+  })
+
+  it('accepts category input', () => {
+    const category = 'Restaurants'
+    cy.visit('/')
+
+    cy.get('#category')
+      .type(category)
+      .should('have.value', category)
+  })
+
+  it('accepts price input', () => {
+    const price = '$$'
+    cy.visit('/')
+    
+    cy.get('#price').click()
+    cy.contains(price).click()
+      .should('have.text', price)
   })
   
 })
